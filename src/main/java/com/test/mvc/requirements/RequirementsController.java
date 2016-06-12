5cc1975d48fe3e4b89d87835a5c6e6601794ddb1
Yang <xin.yang3@hpe.com>
2016-06-12 14:56:17 +0800

    merge by yxin by 20160612

--- src/main/java/com/test/mvc/requirements/RequirementsController.java
+++ src/main/java/com/test/mvc/requirements/RequirementsController.java
@@ -1,29 +1,34 @@
 package com.test.mvc.requirements;
 
+import com.jfinal.aop.Before;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 import com.platform.constant.ConstantInit;
 import com.platform.mvc.base.BaseController;
-
 import org.apache.log4j.Logger;
-import com.jfinal.aop.Before;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * XXX 管理 描述：
+ * XXX 管理	
+ * 描述：
  * 
- * /jf/test/requirements /jf/test/requirements/save /jf/test/requirements/edit
- * /jf/test/requirements/update /jf/test/requirements/view
- * /jf/test/requirements/delete /common/requirements/add.html
+ * /jf/test/requirements
+ * /jf/test/requirements/save
+ * /jf/test/requirements/edit
+ * /jf/test/requirements/update
+ * /jf/test/requirements/view
+ * /jf/test/requirements/delete
+ * /common/requirements/add.html
  * 
  */
-// @Controller(controllerKey = "/jf/test/requirements")
+//@Controller(controllerKey = "/jf/test/requirements")
 public class RequirementsController extends BaseController {
 
 	@SuppressWarnings("unused")
 	private static Logger log = Logger.getLogger(RequirementsController.class);
-
+	
 	/**
 	 * 列表
 	 */
@@ -31,7 +36,7 @@ public class RequirementsController extends BaseController {
 		paging(ConstantInit.db_dataSource_main, splitPage, Requirements.sqlId_splitPage_from);
 		render("/test/requirements/list.html");
 	}
-
+	
 	/**
 	 * 保存
 	 */
@@ -40,7 +45,7 @@ public class RequirementsController extends BaseController {
 		getModel(Requirements.class).save();
 		render("/test/requirements/add.html");
 	}
-
+	
 	/**
 	 * 准备更新
 	 */
@@ -50,7 +55,7 @@ public class RequirementsController extends BaseController {
 		setAttr("requirements", requirements);
 		render("/test/requirements/update.html");
 	}
-
+	
 	/**
 	 * 更新
 	 */
@@ -68,18 +73,48 @@ public class RequirementsController extends BaseController {
 		setAttr("requirements", requirements);
 		render("/test/requirements/view.html");
 	}
-
+	
 	/**
 	 * 删除
 	 */
 	public void delete() {
-		RequirementsService.service.delete("requirements", "req_sn", getPara() == null ? ids : getPara());
-		redirect("/jf/test/requirements");
+//		RequirementsService.service.delete("requirements", "req_sn", getPara() == null ? ids : getPara());
+//		redirect("/jf/test/requirements");
 	}
 
 	public void reportone() {
 		List<Record> data = Db.find("select count(*) as 数量,creationdate as 日期 from requirements group by creationdate");
-		renderJson("testdata", data);
+		renderJson("testdata",data);
 	}
 
+	public void reqcover() {
+		long total = Db.queryLong("select count(*) from requirements");
+		long covered = Db.queryLong("select count(*) from requirements where req_status != 'Not Covered' or req_status is not null");
+		Record record = new Record();
+		record.set("数量",covered);
+		record.set("类型","覆盖数");
+		Record recordtwo = new Record();
+		recordtwo.set("数量",total-covered);
+		recordtwo.set("类型","非覆盖数");
+		List<Record> lor = new ArrayList<>();
+		lor.add(record);
+		lor.add(recordtwo);
+		renderJson("data",lor);
+	}
+
+	public void reqpass() {
+		long total = Db.queryLong("select count(*) from requirements");
+		long pass = Db.queryLong("select count(*) from requirements where req_status = 'Passed'");
+		Record record = new Record();
+		record.set("数量",pass);
+		record.set("类型","通过数");
+		Record recordtwo = new Record();
+		recordtwo.set("数量",total-pass);
+		recordtwo.set("类型","非通过数");
+		List<Record> lor = new ArrayList<>();
+		lor.add(record);
+		lor.add(recordtwo);
+		renderJson("data",lor);
+	}
+	
 }
--- src/main/webapp/jsFile/testingHPE/DimpleForContent.js
+++ src/main/webapp/jsFile/testingHPE/DimpleForContent.js
@@ -7,23 +7,40 @@ var DimpleInContent = function() {
     //For the Ring
     var ringDimpleContent = function() {
         var svg = dimple.newSvg("#dimpleRingContent", 590, 400);
-        data = [
-            { "系统":"结算系统","缺陷统计":1000 },
-            { "系统":"前台UI","缺陷统计":1200 }
-        ];
-        var myChart = new dimple.chart(svg, data);
-        myChart.setBounds(20, 20, 460, 360);
-        myChart.addMeasureAxis("p", "缺陷统计");
-        var ring = myChart.addSeries("系统", dimple.plot.pie);
-        ring.innerRadius = "50%";
-        myChart.addLegend(500, 20, 90, 300, "left");
-        myChart.draw();
+        url = 'http://localhost:8080/jf/test/requirements/reqcover';
+        d3.json(url,function(jsonstr) {
+            data = jsonstr.data;
+            var myChart = new dimple.chart(svg, data);
+            myChart.setBounds(20, 20, 460, 360);
+            myChart.addMeasureAxis("p", "数量");
+            var ring = myChart.addSeries("类型", dimple.plot.pie);
+            ring.innerRadius = "50%";
+            myChart.addLegend(500, 20, 90, 300, "left");
+            myChart.draw();
+        })
+    };
+
+    //For the Ring
+    var ringDimpleContenttwo = function() {
+        var svg = dimple.newSvg("#dimpleRingContenttwo", 590, 400);
+        url = 'http://localhost:8080/jf/test/requirements/reqpass';
+        d3.json(url,function(jsonstr) {
+            data = jsonstr.data;
+            var myChart = new dimple.chart(svg, data);
+            myChart.setBounds(20, 20, 460, 360);
+            myChart.addMeasureAxis("p", "数量");
+            var ring = myChart.addSeries("类型", dimple.plot.pie);
+            ring.innerRadius = "50%";
+            myChart.addLegend(500, 20, 90, 300, "left");
+            myChart.draw();
+        })
     };
 
     return {
         //main function to initiate template pages
         init: function() {
             ringDimpleContent();
+            ringDimpleContenttwo();
         }
     };
 
